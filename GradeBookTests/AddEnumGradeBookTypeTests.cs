ee34b021fcbf6810cb099ba7f270f75b6de7f0f6
freelance.dom@gmail.com <freelance.dom@gmail.com>
2018-07-22 00:15:12 +0100

    Creating New GradeBookTypes

---
+++ GradeBookTests/AddEnumGradeBookTypeTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Linq;
+using GradeBook.UserInterfaces;
+using Xunit;
+
+
+namespace GradeBookTests
+{
+public enum AddEnumGradeBookTypeTests {
+Standard,
+Ranked,
+ESNU
+
+
+}
+
+}
